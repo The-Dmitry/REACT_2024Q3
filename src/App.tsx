1c2517d3a4d1074b391c8a732d2b1208d70e7cae
the-dmitry <dzmitrygaponenko@gmail.com>
2024-07-03 01:52:51 +0300

    feat: add loader

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./core/components/header/Header";
 import Card from "./models/CardData";
 import Main from "./core/components/main/Main";
+import Loader from "./shared/components/loader/Loader";
 
 export interface ApiResponse {
   count: number;
@@ -12,36 +13,42 @@ export interface ApiResponse {
 }
 
 interface AppState {
-  initialValue: string;
+  searchValue: string;
   data: Card[];
+  isLoading: boolean;
 }
 
 export default class App extends Component<undefined, AppState> {
   state = {
-    initialValue: "",
+    searchValue: "",
     data: [],
+    isLoading: false,
   };
 
   render(): ReactNode {
     return (
       <>
         <Header submit={(value) => this.getData(value)} />
-        <Main data={this.state.data} />
+        {this.state.isLoading ? <Loader /> : <Main data={this.state.data} />}
       </>
     );
   }
 
   async getData(query?: string) {
-    console.log(query);
+    try {
+      this.setState({ isLoading: true });
+      const response = await fetch(
+        `https://swapi.dev/api/people/?page=1${query ? `&search=${query.trim()}` : ""}`
+      );
 
-    const response = await fetch(
-      `https://swapi.dev/api/people/?page=1${query ? `&search=${query.trim()}` : ""}`
-    );
-
-    if (response.ok) {
-      const data = await response.json();
-      this.setState({ data: data.results });
-      console.log(this.state.data);
+      if (response.ok) {
+        const data: ApiResponse = await response.json();
+        this.setState({ data: data.results });
+      }
+    } catch (e) {
+      console.log(e);
+    } finally {
+      this.setState({ isLoading: false });
     }
   }
 }
---
+++ src/shared/components/loader/Loader.tsx
@@ -0,0 +1,12 @@
+import styles from "./loader.module.css";
+import { Component, ReactNode } from "react";
+
+export default class Loader extends Component {
+  render(): ReactNode {
+    return (
+      <div className={styles.loader}>
+        <div className={styles.spinner}></div>
+      </div>
+    );
+  }
+}
