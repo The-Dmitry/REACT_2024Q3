a5ea06f4d003d224a14f6516a9f568608ee1ee7f
the-dmitry <dzmitrygaponenko@gmail.com>
2024-07-03 01:08:07 +0300

    feat: add placeholder for cases where there is no data

--- src/core/components/main/Main.tsx
+++ src/core/components/main/Main.tsx
@@ -11,11 +11,16 @@ export default class Main extends Component<MainProps> {
   render(): ReactNode {
     return (
       <main className={styles.main}>
-        <ul className={styles.list}>
-          {this.props.data.map((info) => (
-            <Card key={info.name} {...info} />
-          ))}
-        </ul>
+        {!!this.props.data.length && (
+          <ul className={styles.list}>
+            {this.props.data.map((info) => (
+              <Card key={info.name} {...info} />
+            ))}
+          </ul>
+        )}
+        {!this.props.data.length && (
+          <h2 className={styles.placeholder}>Nothing Found</h2>
+        )}
       </main>
     );
   }
