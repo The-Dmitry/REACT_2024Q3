5c459bb82231f59f7ae97025e666562e6cfe996c
the-dmitry <dzmitrygaponenko@gmail.com>
2024-07-04 22:56:08 +0300

    feat: add image to card

--- src/shared/components/card/Card.tsx
+++ src/shared/components/card/Card.tsx
@@ -4,8 +4,11 @@ import CardData from "../../../models/CardData";
 
 export default class Card extends Component<CardData> {
   render(): ReactNode {
+    const id = this.props.url.replace(/[^\d]/g, "");
+    const url = `https://starwars-visualguide.com/assets/img/characters/${id}.jpg`;
     return (
       <li className={styles.card}>
+        <img className={styles.image} src={url} alt={this.props.name} />
         <h3>{this.props.name}</h3>
         <p>Gender: {this.props.gender}</p>
         <p>Yey color: {this.props.eye_color}</p>
